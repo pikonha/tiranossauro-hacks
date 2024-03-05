666e3eb6e9cccb213a7de63584eef501b91d4000
picolloo <lucaspicolloo@gmail.com>
2024-03-05 16:48:40 -0300

    implementando a interface

--- src/memory_repo.ts
+++ src/memory_repo.ts
@@ -3,11 +3,15 @@ import { Exercicio } from "./types";
 export class RepositorioMemoria {
   constructor() {}
 
-  async create(entity: Exercicio) {}
+  async create(entity: Exercicio): Promise<Exercicio> {
+    return;
+  }
 
-  async read(id: number) {}
+  async read(id: number): Promise<Exercicio | undefined> {
+    return;
+  }
 
-  async update(id: number, updatedEntity: Partial<Exercicio>) {}
+  async update(id: number, updatedEntity: Partial<Exercicio>): Promise<void> {}
 
-  async delete(id: number) {}
+  async delete(id: number): Promise<void> {}
 }
